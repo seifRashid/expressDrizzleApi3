0076501949ce6b31fc6dd1fcc50db3292649225d
Rashid Seif <rseif1218@gmail.com>
2026-03-29 18:03:02 +0300

    Create login route with its functionality

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -6,20 +6,19 @@ import { db } from "../db.ts";
 import { usersTable } from "../schema.ts";
 import bcrypt from "bcryptjs";
 
-export const registerUserController = async (req: Request, res: Response) => {
+export const register = async (req: Request, res: Response) => {
   try {
     const { name, secName, age, email, password } = req.body;
 
-    //check if user already exists
-    // const existingUser = await db
-    //   .select()
-    //   .from(usersTable)
-    //   .where(eq(usersTable.email, email))
-    //   .limit(1);
-    //   console.log("Existing user query result:", existingUser);
-    // if (existingUser) {
-    //   return res.status(400).json({ message: "User already exists" });
-    // }
+    // check if user already exists
+    const existingUser = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.email, email))
+      .limit(1);
+    if (existingUser.length > 0) {
+      return res.status(400).json({ message: "User already exists" });
+    }
     //hash the password before storing it in the database
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await db
@@ -35,10 +34,52 @@ export const registerUserController = async (req: Request, res: Response) => {
     res.status(201).json({
       success: true,
       message: "User registered successfully!",
-      user: newUser,
+      data:{
+        user: {
+        id: newUser[0].id,
+        email,
+        name,
+      },
+      }
     });
   } catch (error) {
     console.error("Error registering user:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+//login user controller function
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    // check if user exists
+    const user = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.email, email))
+      .limit(1);
+      console.log("User found:", user);
+    if (user.length === 0) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    //compare the password with the hashed password in the database
+    const isPasswordValid = await bcrypt.compare(password, user[0].password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    res.status(200).json({
+      success: true,
+      message: "User logged in successfully!",
+      data: {
+        user: {
+        id: user[0].id,
+        email,
+      },
+      },
+    });
+  } catch (error) {
+    console.error("Error logging in user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,30 +2,13 @@
 import { Router } from "express";
 import { db } from "../db.ts";
 import { usersTable } from "../schema.ts";
-import { registerUserController } from "../controllers/authController.ts";
+import { login, register } from "../controllers/authController.ts";
 const router = Router();
 
 //register user
-router.post("/register",registerUserController);
+router.post("/register", register);
 
 //login user
-router.post("/login", async (req, res) => {
-  try {
-    const { email } = req.body;
-    const user = await db
-      .select()
-      .from(usersTable)
-      .where(usersTable.email.equals(email))
-      .first();
-    if (user) {
-      res.status(200).json({ message: "Login successful!", user });
-    } else {
-      res.status(401).json({ message: "Invalid email or password" });
-    }
-  } catch (error) {
-    console.error("Error logging in user:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
+router.post("/login", login);
 
 export default router;
